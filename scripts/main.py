f73745c5f5e653070e6c864aa03c621bd359ed67
k4nkan <kanta.y.0126@gmail.com>
2025-11-15 02:59:15 +0900

    add: get repo info

--- scripts/main.py
+++ scripts/main.py
@@ -8,6 +8,32 @@ GITHUB_API_TOKEN = os.environ["GITHUB_API_TOKEN"]
 HEADERS = {"Authorization": f"Bearer {GITHUB_API_TOKEN}"}
 
 
+def get_repo_info(user: str, repo: str) -> dict:
+    """func for get repo data"""
+    url = f"https://api.github.com/repos/{user}/{repo}"
+
+    try:
+        res = requests.get(url, headers=HEADERS, timeout=10).json()
+        result = {
+            key: res[key]
+            for key in [
+                "name",
+                "full_name",
+                "description",
+                "html_url",
+                "stargazers_count",
+                "forks_count",
+                "open_issues_count",
+            ]
+            if key in res
+        }
+        return result
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
 def get_repo_tree(user: str, repo: str) -> dict:
     """func for get repo data and tree data"""
     url = f"https://api.github.com/repos/{user}/{repo}/git/trees/main?recursive=1"
@@ -41,9 +67,11 @@ def main() -> None:
     user_name = args[1]
     repo_name = args[2]
 
+    info = get_repo_info(user_name, repo_name)
     structure = get_repo_tree(user_name, repo_name)
 
-    print(structure)
+    print(f"info : {info}\n")
+    print(f"structure data : {structure}\n")
 
 
 if __name__ == "__main__":
